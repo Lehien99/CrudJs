4ab30ed2bf530beb3c101b85454b29bad68b5331
Lehien99 <56247237+Lehien99@users.noreply.github.com>
2022-06-23 08:23:59 +0700

    Basic Html/Css

---
+++ index.js
@@ -0,0 +1,11 @@
+const userName = document.getElementById('userName');
+const fullName = document.getElementById('fullName');
+const email = document.getElementById('Email');
+const birthday = document.getElementById("birthday");
+const submit = document.getElementById("btn-save");
+
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+  
